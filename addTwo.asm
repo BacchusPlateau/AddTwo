3a5613cbd50e3f31f4b732ab3b350debfc8fbe75
BacchusPlateau <straightener@hotmail.com>
2026-06-05 18:26:27 -0700

    add two numbers

---
+++ addTwo.asm
@@ -0,0 +1,57 @@
+
+number1 = 26
+number2 = 55
+addResult = $8D
+
+        icl 'equates.asm'
+        icl 'routines.asm'
+
+        org $2000
+
+        .proc main
+    
+        ;=============================================================
+        ; print a string example
+        ;
+        mva #1 csrhinh                  ; hide the cursor
+        mva #6 rowcrs                   ; set output row
+        mva #10 colcrs                  ; set output column
+        
+        mva #<string1 strptr_lo         ; low byte of string1 address
+        mva #>string1 strptr_hi         ; high byte o f string1 address
+        jsr print_string                ; print the string
+
+        ; add two numbers
+        lda #number1        ; A = literal value in number1
+        clc                 ; clear the carry
+        adc #number2        ; A = A + liter value in number 2
+        sta addResult       ; memory[addResult] = A
+
+        ;print both digits, from left to right 
+        ;first digit, shift right four times to move the first digit to the second digit's place
+        lsr 
+        lsr
+        lsr
+        lsr 
+        adc #offset_to_char   
+        jsr putchar
+
+        lda addResult       ; A = result from above
+        and #%00001111      ; mask bottom 4 bits to obtain just the right digit
+        adc #offset_to_char
+        jsr putchar
+
+        jmp stop                        ; stop is inside routines.asm
+        ;=============================================================
+
+                .endp
+
+;===================================================================
+; Data section
+;===================================================================
+        .local string1
+        .byte 'RESULT=',0
+        .endl
+
+
+        run main
